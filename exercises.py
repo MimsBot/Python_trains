5a629244023d425eb480d1ae54bce844e2363dc1
http://www.github.com/mimsbot <mimseyfield@gmail.com>
2019-02-15 15:43:13 -0500

    first commit

---
+++ exercises.py
@@ -0,0 +1,43 @@
+fav_colors = ['teal', 'purple', 'green']
+age_friends_me = [24, 44, 32]
+coin_flip = [0, 0, 1, 1, 1]
+favorite_consoles = ['sega', 'nintendo', 'playstation']
+
+print(coin_flip)
+
+print(fav_colors[0])
+
+age_friends_me.sort()
+print(age_friends_me)
+
+age_friends_me.append(0)
+print(age_friends_me)
+
+print(1)
+
+
+word_dict = {
+    'cheese': 'delicious',
+    'bread': 'good with cheese',
+    'butter': 'good with bread',
+}
+
+movie_dict = {
+    'finding_nemo': '2003',
+    'drunken_master': '1978',
+    'mars_attacks': '1996',
+}
+
+cities_dict = {
+    'wawa': '2975',
+    'jeju_island': '604771',
+    'bora_bora': '10605',
+}
+
+ages_dict = {
+    'mack': '24',
+    'chad': '44',
+    'mims': '32',
+}
+
+movie_dict.values()
